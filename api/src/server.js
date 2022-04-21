1be7dc796e80e85aeaf3485b37f1432c9dbdbafd
Artem Urazgulov <enterescaper@protonmail.com>
2022-04-21 19:30:25 +0300

    setup simple routes

--- api/src/server.js
+++ api/src/server.js
@@ -6,6 +6,26 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+app.get('/api/v1/reviews', (req, res) => {
+  res.status(200).json({ success: true, data: 'List all reviews' })
+})
+
+app.get('/api/v1/reviews/:id', (req, res) => {
+  res.status(200).json({ success: true, data: `Get review ${req.params.id}` })
+})
+
+app.post('/api/v1/reviews', (req, res) => {
+  res.status(200).json({ success: true, data: 'Create new review' })
+})
+
+app.put('/api/v1/reviews/:id', (req, res) => {
+  res.status(200).json({ success: true, data: `Update review ${req.params.id}` })
+})
+
+app.delete('/api/v1/reviews/:id', (req, res) => {
+  res.status(200).json({ success: true, data: `Delete review ${req.params.id}` })
+})
+
 const PORT = process.env.PORT || 4000
 const ENV = process.env.NODE_ENV
 
