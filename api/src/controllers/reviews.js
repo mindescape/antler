2c94ccd73ae4626447b83a70c6503ac6252791ab
Artem Urazgulov <enterescaper@protonmail.com>
2022-05-01 20:02:31 +0300

    add async middleware

--- api/src/controllers/reviews.js
+++ api/src/controllers/reviews.js
@@ -1,98 +1,79 @@
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 const Review = require('../models/Review')
 
 // @desc Get all reviews
 // @route GET /api/v1/reviews
 // @access Public
-exports.getReviews = async (req, res, next) => {
-  try {
-    const reviews = await Review.find()
+exports.getReviews = asyncHandler(async (req, res, next) => {
+  const reviews = await Review.find()
 
-    res.status(200).json({
-      success: true,
-      data: reviews,
-      count: reviews.length,
-    })
-  } catch (err) {
-    next(err)
-  }
-}
+  res.status(200).json({
+    success: true,
+    data: reviews,
+    count: reviews.length,
+  })
+})
 
 // @desc Get single review
 // @route GET /api/v1/reviews/:id
 // @access Public
-exports.getReview = async (req, res, next) => {
-  try {
-    const review = await Review.findById(req.params.id)
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id)
 
-    if (!review) {
-      return next(new ErrorResponse(`Review with id ${req.params.id} has not been found`, 404))
-    }
-
-    res.status(200).json({
-      success: true,
-      data: review,
-    })
-  } catch (err) {
-    next(err)
+  if (!review) {
+    return next(new ErrorResponse(`Review with id ${req.params.id} has not been found`, 404))
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  })
+})
 
 // @desc Create review
 // @route POST /api/v1/reviews/
 // @access Private
-exports.createReview = async (req, res, next) => {
-  try {
-    const review = await Review.create(req.body)
+exports.createReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.create(req.body)
 
-    res.status(201).json({
-      success: true,
-      data: review,
-    })
-  } catch (err) {
-    next(err)
-  }
-}
+  res.status(201).json({
+    success: true,
+    data: review,
+  })
+})
 
 // @desc Update review
 // @route PUT /api/v1/reviews/:id
 // @access Private
-exports.updateReview = async (req, res, next) => {
-  try {
-    const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    })
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
 
-    if (!review) {
-      return next(new ErrorResponse(`Review with id ${req.params.id} has not been found`, 404))
-    }
-
-    res.status(200).json({
-      success: true,
-      data: review,
-    })
-  } catch (err) {
-    next(err)
+  if (!review) {
+    return next(new ErrorResponse(`Review with id ${req.params.id} has not been found`, 404))
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  })
+})
 
 // @desc Delete review
 // @route DELETE /api/v1/reviews/:id
 // @access Private
-exports.deleteReview = async (req, res, next) => {
-  try {
-    const review = await Review.findByIdAndDelete(req.params.id)
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findByIdAndDelete(req.params.id)
 
-    if (!review) {
-      return next(new ErrorResponse(`Review with id ${req.params.id} has not been found`, 404))
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {},
-    })
-  } catch (err) {
-    next(err)
+  if (!review) {
+    return next(new ErrorResponse(`Review with id ${req.params.id} has not been found`, 404))
   }
-}
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  })
+})
